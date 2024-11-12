392fa4855b6c8b3a4d0757e8c43cf49f46335aed
bcrhbrhcdb <apet2804@mpsedu.org>
2024-11-11 18:08:06 -0600

    hbyubugbtubgu

---
+++ assets/scripts/main.js
@@ -0,0 +1,35 @@
+class Game{
+    constructor(canvas, context){
+        this.canvas = canvas;
+        this.ctx = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.player = new Player(this);
+
+        window.addEventListener("resize", function(e){
+            console.log(e)
+        })
+    };
+    render(){
+        this.ctx.fillStyle = 'red';
+        this.player.update()
+        this.player.draw();
+    }
+}
+
+window.addEventListener('load', function(){
+    const canvas = this.document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 720;
+    canvas.height = 720;
+    
+    const game = new Game(canvas, ctx);
+    game.render();
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        game.render();
+        requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+})
